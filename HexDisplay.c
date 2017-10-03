7d3e6ee84ed5b363f4feb6a1a0c658242114dc98
ecotellessa <31664456+ecotellessa@users.noreply.github.com>
2017-10-03 14:33:12 -0400

    Add files via upload
    made changes to buttonirs interruptdemo and hexdisplay

--- HexDisplay.c
+++ HexDisplay.c
@@ -4,16 +4,23 @@
  *  Created on: Apr 21, 2015
  *      Author: brent.horine
  */
+#include "alt_types.h"
+#include "io.h"
+#include "system.h"
 #include "seven_seg_hex.h"
 #include "seven_seg_bcd.h"
 
-extern volatile alt_u8 leading_zeros;
-extern volatile alt_u8 display_as_bcd;
+ extern volatile alt_u8 leading_zeros;
+ extern volatile alt_u8 display_as_bcd;
 
+void toggleBCDHEX(){
+	display_as_bcd = !display_as_bcd;
+}
 void HexDisplay(alt_u32 * base, alt_u16 num) {
-	if (display_as_bcd) {
-		seven_seg_bcd(base, num, leading_zeros);
+	leading_zeros = 0;
+	if (display_as_bcd){
+		seven_seg_bcd(base,num,leading_zeros);
 	} else {
-		seven_seg_hex(base, num, leading_zeros);
+		seven_seg_hex(base, num, 0);// leading_zeros);
 	}
 }
--- InterruptDemo.c
+++ InterruptDemo.c
@@ -2,26 +2,44 @@
 #include "io.h"
 #include "system.h"
 #include "alt_types.h"
+#include "sys\alt_irq.h"
+#include "timerISR.h"
 
 /*******************************************************************************
  * int main()                                                                  *
  *                                                                             *
  ******************************************************************************/
 
+volatile alt_u8 leading_zeros = 0x01&IORD(SLIDER_SWITCHES_BASE,0);
+
 int main(void)
 { 
     /* Setup */
+	alt_u32 * pCount;
+	static alt_u32 count = 0;
+	pCount = &count;
 
     /* Initialize Variables */
 
     /* Register ISRs */
-
+	//alt_irq_register(PUSHBUTTONS_IRQ, ?, buttonISR);
+	alt_irq_register(INTERVAL_TIMER_IRQ, (void*)pCount, timerISR);
     /* Initialize Timer */
+	// Setup start value
+    IOWR(INTERVAL_TIMER_BASE, 2, 0xFFFF&INTERVAL_TIMER_FREQ);
+    IOWR(INTERVAL_TIMER_BASE, 3, 0xFFFF&(INTERVAL_TIMER_FREQ >> 16));
+    // Start the timer
+    IOWR(INTERVAL_TIMER_BASE, 1, 0x7); // Stop=0, Start=1, Cont=1, ITO=1 => 0111b = 0x7
+
+    // Initialize the pushbuttons
+    IOWR(PUSHBUTTONS_BASE, 2, 0XE); //1110b=E because pushbutton 0 is reset
+    // Register the pushbutton ISR
+
 
     /* Loop while processing interrupts */
     while( 1 ) 
     {
-
+    	//IOWR(RED_LEDS_BASE 0)
     }
     return 0; // should never get here
 }
